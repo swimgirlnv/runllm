4a080441a0f554fe7c1d947be32bab4e42d26adc
Becca <rebecca_waterson@brown.edu>
2024-12-04 00:58:05 -0500

    mayb

--- my-app/src/models/RoomModel.tsx
+++ my-app/src/models/RoomModel.tsx
@@ -1,16 +1,8 @@
 import { useGLTF } from '@react-three/drei';
-import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
-
-type GLTFResult = GLTF & {
-  nodes: any;
-  materials: any;
-};
-
-useGLTF.preload('/room3.glb');
 
 function RoomModel(props: any): JSX.Element {
-  const { scene } = useGLTF('/room3.glb') as GLTFResult;
+  const { scene } = useGLTF('/room3.glb');
   return <primitive object={scene} {...props} />;
-}
+};
 
 export default RoomModel;
