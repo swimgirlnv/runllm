0a2bdf72e8d04faf17b2e9529cb296d6018a141d
Becca <rebecca_waterson@brown.edu>
2024-12-03 23:48:38 -0500

    trying?

--- runllm/src/components/RoomModel.jsx
+++ runllm/src/components/RoomModel.jsx
@@ -1,7 +1,8 @@
 import { useGLTF } from "@react-three/drei";
 
+
 function RoomModel(props) {
-  const { scene } = useGLTF("/models/room3.glb");
+  const { scene } = useGLTF("/room3.glb");
   return (
     <primitive object={scene} {...props} />
   );
